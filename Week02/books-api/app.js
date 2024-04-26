608f5d35891590cf6c22dbc1c0ad58375a05a63e
Liew Zhan Yang <148023902+LiewZhanYang@users.noreply.github.com>
2024-04-26 21:41:07 +0800

    complete part 3

--- Week02/books-api/app.js
+++ Week02/books-api/app.js
@@ -34,32 +34,32 @@ app.get("/books/:id", (req, res) => {
   }
 });
 
-app.put('/books/:id', (req, res) => {
+app.put("/books/:id", (req, res) => {
   const bookId = parseInt(req.params.id); // Get book ID from URL parameter
   const updatedBook = req.body; // Get updated book data from request body
 
-  const bookIndex = books.findIndex(book => book.id === bookId);
+  const bookIndex = books.findIndex((book) => book.id === bookId);
 
   if (bookIndex !== -1) {
     updatedBook.id = bookId;
     books[bookIndex] = updatedBook; // Update book data in the array
     res.json(updatedBook); // Send updated book data
   } else {
-    res.status(404).send('Book not found'); // Send error for non-existent book
+    res.status(404).send("Book not found"); // Send error for non-existent book
   }
 });
 
-//app.delete('/books/:id',(req,res) => {
- // const bookId = parseInt(req.params.id);
- // const bookIndex = books.findIndex(book => book.id =)
+app.delete("/books/:id", (req, res) => {
+  const bookId = parseInt(req.params.id);
+  const bookIndex = books.findIndex((book) => book.id == bookId);
 
-
-
-
-
-
-
-
+  if (bookIndex !== -1) {
+    books.splice(bookIndex, 1);
+    res.status(200).send();
+  } else {
+    res.status(404).send("Book not found");
+  }
+});
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
