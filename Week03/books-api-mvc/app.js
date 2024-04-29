d1ac08002e7c0d0982463835c65a105f28015aa9
Liew Zhan Yang <148023902+LiewZhanYang@users.noreply.github.com>
2024-04-29 11:06:07 +0800

    doing part 8

--- Week03/books-api-mvc/app.js
+++ Week03/books-api-mvc/app.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const booksController = require("./controllers/booksController"); // Import controllers
+const validateBook = require("./middlewares/validateBook");
 
 const app = express();
 
 app.use(bodyParser.json()); // Parse incoming JSON data in request body
 app.use(bodyParser.urlencoded({ extended: true })); // For form data handling
+app.post("/books", validateBook, booksController.createBook); // Add validateBook before createBook
+app.put("/books/:id", validateBook, booksController.updateBook); // Add validateBook before updateBook
 
 // Define individual routes for each controller function
 app.get("/books", booksController.getAllBooks);
