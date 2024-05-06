b5e322b7854f6dffbc19a9d0eb00a77e434156dd
Liew Zhan Yang <148023902+LiewZhanYang@users.noreply.github.com>
2024-05-06 11:12:24 +0800

    doing part 5

--- Week04/book-api-mvc-db/dbConfig.js
+++ Week04/book-api-mvc-db/dbConfig.js
@@ -1,6 +1,6 @@
 module.exports = {
   user: "booksapi_user", // Replace with your SQL Server login username
-  password: "123456789", // Replace with your SQL Server login password
+  password: "123", // Replace with your SQL Server login password
   server: "localhost",
   database: "bed_db",
   trustServerCertificate: true,
